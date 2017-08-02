61be9e939bdbef50c5d05f1a695f5b8996147f09
slzn <slzn0124@gmail.com>
2017-08-02 11:33:28 +0800

    Add project files.

---
+++ TshockLogs_Explosives/Explosives_Config.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace TshockLogs
+{
+    class Explosives_Config
+    {
+        [JsonProperty]
+        public bool ShowOnConsole = false;
+
+        [JsonProperty]
+        public string[] Explosives = { "Bomb", "BouncyBomb" , "StickyBomb",
+                                        "Dynamite", "BouncyDynamite", "StickyDynamite",
+                                        "BombFish", "Explosives", "ExplosiveBunny"};
+
+        internal static Explosives_Config Read(string path)
+        {
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("Using default configs");
+                Explosives_Config config = new Explosives_Config();
+                config.Write(path);
+                return config;
+            }
+            using (var fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                return Read(fs);
+            }
+        }
+
+        internal static Explosives_Config Read(Stream stream)
+        {
+            using (var sr = new StreamReader(stream))
+            {
+                return JsonConvert.DeserializeObject<Explosives_Config>(sr.ReadToEnd());
+            }
+        }
+
+        internal void Write(string path)
+        {
+            using (var fs = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.Write))
+            {
+                Write(fs);
+            }
+        }
+
+        internal void Write(Stream stream)
+        {
+            var str = JsonConvert.SerializeObject(this, Formatting.Indented);
+            using (var sw = new StreamWriter(stream))
+            {
+                sw.Write(str);
+            }
+        }
+    }
+}
---
+++ TshockLogs_Explosives/TshockLogs_Explosives.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using TShockAPI;
+using TShockAPI.Hooks;
+using Terraria;
+using Terraria.ID;
+using TerrariaApi.Server;
+using System.IO;
+
+namespace TshockLogs
+{
+    [ApiVersion(2, 1)]
+    public class Explosives: TerrariaPlugin
+    {
+        private string ConfigFilePath = Path.Combine(TShock.SavePath, "TshockLogs_Explosives_Config.json");
+        private Explosives_Config ExplosivesConfig;
+        private Dictionary<short, string> InterestingExplosives;
+
+        /// <summary>
+      	/// Gets the author(s) of this plugin
+        /// </summary>
+      	public override string Author
+        {
+            get { return "slzn0124@gmail.com"; }
+        }
+
+        /// <summary>
+        /// Gets the description of this plugin.
+        /// A short, one lined description that tells people what your plugin does.
+        /// </summary>
+        public override string Description
+        {
+            get { return "Add explosives usage logs"; }
+        }
+
+        /// <summary>
+        /// Gets the name of this plugin.
+        /// </summary>
+        public override string Name
+        {
+            get { return "TshockLogs_Explosives"; }
+        }
+
+        /// <summary>
+        /// Gets the version of this plugin.
+        /// </summary>
+        public override Version Version
+        {
+            get { return new Version(1, 0, 0, 0); }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the TestPlugin class.
+        /// This is where you set the plugin's order and perfrom other constructor logic
+        /// </summary>
+        public Explosives(Main game) : base(game)
+        {
+            ExplosivesConfig = Explosives_Config.Read(ConfigFilePath);
+            SetupExplosivesDictionary();
+        }
+
+        /// <summary>
+        /// Handles plugin initialization. 
+        /// Fired when the server is started and the plugin is being loaded.
+        /// You may register hooks, perform loading procedures etc here.
+        /// </summary>
+        public override void Initialize()
+        {
+            GetDataHandlers.NewProjectile += OnNewProjectile;
+            GeneralHooks.ReloadEvent += OnReload;
+        }
+
+
+
+        private void OnNewProjectile(object sender, TShockAPI.GetDataHandlers.NewProjectileEventArgs args)
+        {
+            string explosiveName = "";
+            if (InterestingExplosives.TryGetValue(args.Type,out explosiveName))
+            {                
+                TSPlayer player = TShock.Players[args.Owner];
+                if (ExplosivesConfig.ShowOnConsole)
+                {
+                    Console.WriteLine("{0} throws {1} at ({2},{3})", player.Name, explosiveName, args.Position.X, args.Position.Y);
+                }
+                
+                TShock.Log.Info("{0} throws {1} at ({2},{3})", player.Name, explosiveName, args.Position.X, args.Position.Y);
+            }
+            
+        }
+
+        private void OnReload(ReloadEventArgs reloadEventArgs)
+        {
+            Console.WriteLine("Reload TshockLogs_Explosives_Config.json");
+            ExplosivesConfig = Explosives_Config.Read(ConfigFilePath);
+            SetupExplosivesDictionary();
+        }
+
+        private void SetupExplosivesDictionary()
+        {
+            InterestingExplosives = new Dictionary<short, string>();
+            ProjectileID projectileId = new ProjectileID();
+            Type type = projectileId.GetType();
+            foreach (string explosive in ExplosivesConfig.Explosives)
+            {
+                short id = (short) type.GetField(explosive).GetValue(projectileId);
+                InterestingExplosives.Add(id, explosive);
+            }
+        }
+
+        /// <summary>
+        /// Handles plugin disposal logic.
+        /// *Supposed* to fire when the server shuts down.
+        /// You should deregister hooks and free all resources here.
+        /// </summary>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                GetDataHandlers.NewProjectile -= OnNewProjectile;
+                GeneralHooks.ReloadEvent -= OnReload;
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
